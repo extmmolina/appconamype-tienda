ee5b784aa037db549bb8a75ee3263858cd2c0ad0
mmolina <ext_mmolina@conamype.gob.sv>
2022-01-24 13:39:37 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'appconamype tienda',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
